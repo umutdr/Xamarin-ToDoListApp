4ec88b559815b8f9227c2f18aeb811c2e5cba260
umutdr <umutdr98@gmail.com>
2021-09-09 22:14:04 +0300

    Add project files.

---
+++ ToDoListApp/ToDoListApp/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListApp
+{
+    public class TodoItem
+    {
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+
+        public TodoItem(string todoText, bool todoCompleted)
+        {
+            this.Text = todoText;
+            this.Completed = todoCompleted;
+        }
+    }
+}
---
+++ ToDoListApp/ToDoListApp/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoListApp.ViewModels
+{
+    public class TodoListViewModel : ContentPage
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public ICommand AddTodoCommand { get; set; }
+        public ICommand RemoveTodoCommand { get; set; }
+
+        string _NewTodoInputValue = string.Empty;
+        public string NewTodoInputValue
+        {
+            get => _NewTodoInputValue;
+            set
+            {
+                if (_NewTodoInputValue != value)
+                {
+                    _NewTodoInputValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            AddTodoCommand = new Command(AddTodoItem);
+            RemoveTodoCommand = new Command(RemoveTodoItem);
+
+            TodoItems.Add(new TodoItem("Add a Todo!", false));
+        }
+
+        void AddTodoItem()
+        {
+
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+            NewTodoInputValue = string.Empty;
+        }
+
+        void RemoveTodoItem(object o)
+        {
+            TodoItems.Remove((TodoItem)o);
+        }
+
+    }
+}
